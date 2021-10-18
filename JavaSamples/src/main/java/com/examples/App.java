cd01faa51bab38320aef41d2e5f10a4f12c590ec
Jithin Iyyani <iyyani.jithin@gmail.com>
2021-10-18 22:21:14 +0530

    Added project setup
    Added project setup

---
+++ JavaSamples/src/main/java/com/examples/App.java
@@ -0,0 +1,13 @@
+package com.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "In this project you will find many java simple programs" );
+    }
+}
---
+++ JavaSamples/src/main/java/com/examples/list/ListExamples.java
@@ -0,0 +1,20 @@
+package com.examples.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExamples {
+
+	
+	
+	
+	public static void main(String[] args) {
+
+		List<String> names = new ArrayList<>();
+		names.add("roger");
+		names.add("rafael");
+		names.add("superman");
+
+	}
+
+}
